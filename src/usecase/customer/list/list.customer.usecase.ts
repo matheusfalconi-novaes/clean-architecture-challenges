1e5817dbb2c6662e180c01e65271c981a10c966c
Matheus Novaes <matheusfalcao.novaes@gmail.com>
2023-06-07 19:22:53 -0300

    Customer Use Cases

---
+++ src/usecase/customer/list/list.customer.usecase.ts
@@ -0,0 +1,35 @@
+import Customer from '../../../domain/customer/entity/customer';
+import CustomerRepositoryInterface from '../../../domain/customer/repository/customer-repository.interface';
+import { InputListCustomerDto, OutputListCustomerDto } from './list.customer.dto';
+
+export default class ListCustomerUseCase {
+    private customerRepository: CustomerRepositoryInterface;
+
+    constructor(customerRepository: CustomerRepositoryInterface) {
+        this.customerRepository = customerRepository;
+    }
+
+    async execute(input: InputListCustomerDto): Promise<OutputListCustomerDto> {
+        const customers = await this.customerRepository.findAll();
+        return OutputMapper.toOuput(customers);
+    }
+
+
+}
+
+class OutputMapper {
+    static toOuput(customer: Customer[]): OutputListCustomerDto {
+        return {
+            customers: customer.map((customer) => ({
+                id: customer.id,
+                name: customer.name,
+                address: {
+                    street: customer.Address.street,
+                    number: customer.Address.number,
+                    zip: customer.Address.zip,
+                    city: customer.Address.city
+                }
+            }))
+        };
+    }
+}
