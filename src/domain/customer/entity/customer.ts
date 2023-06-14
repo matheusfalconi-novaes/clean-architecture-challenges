23bcff301b547416df2f4beadd460e21bcbbbd4d
Matheus Novaes <matheusfalcao.novaes@gmail.com>
2023-06-14 09:39:40 -0300

    Add notification pattern to Customer

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,23 +1,24 @@
+import Entity from '../../@shared/entity/entity.abstract';
+import NotificationError from '../../@shared/notification/notification.error';
 import Address from '../value-object/address';
 
-export default class Customer {
-    private _id: string;
+export default class Customer extends Entity {
     private _name: string;
     private _address: Address | undefined;
     private _active: boolean = false;
     private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
+        super();
         this._id = id;
         this._name = name;
         this.validate();
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.errors);
+        }
     }
 
-    get id(): string {
-        return this._id;
-    }
-
-    get name() : string {
+    get name(): string {
         return this._name;
     }
 
@@ -34,11 +35,17 @@ export default class Customer {
     }
 
     validate() {
-        if(this._id.length === 0) {
-            throw new Error("Id is required");
+        if (this._id.length === 0) {
+            this.notification.addError({
+                context: "customer",
+                message: "Id is required"
+            });
         }
-        if(this._name.length === 0) {
-            throw new Error("Name is required");
+        if (this._name.length === 0) {
+            this.notification.addError({
+                context: "customer",
+                message: "Name is required"
+            });
         }
     }
 
@@ -48,7 +55,7 @@ export default class Customer {
     }
 
     activate() {
-        if(this._address === undefined) {
+        if (this._address === undefined) {
             throw new Error("Address is mandatory to activate a customer");
         }
         this._active = true;
